c73f3293a607db3232724d90e563f1cf033052dc
Alex Baumgertner <alex.baumgertner@gmail.com>
2022-05-23 18:39:46 +0300

    Add google analytics
    https://nextjs.org/docs/messages/next-script-for-ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+
 import 'antd/dist/antd.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6GC4XK3S9Z"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6GC4XK3S9Z');
+        `}
+      </Script>
+    </>
+
+  )
 }
 
 export default MyApp
