87edc2a52b624f50ad3125666938d7fdefbf14b6
alexbaumgertner <alex.baumgertner@gmail.com>
2025-01-25 22:00:07 +0300

    Update libs

--- src/components/GlobalMenu/GlobalMenu.tsx
+++ src/components/GlobalMenu/GlobalMenu.tsx
@@ -1,15 +1,25 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-import classes from './GlobalMenu.module.css'
+import classes from "./GlobalMenu.module.css";
 
 export const GlobalMenu = () => {
   return (
     <menu className={classes.component}>
-      <li><Link href="/news">edu news</Link></li>
-      <li><Link href="/free-universities">free universities programs</Link></li>
-      <li><Link href="/companies">EduTech companies</Link></li>
-      <li><Link href="/systems">systems</Link></li>
-      <li><Link href="/books">books</Link></li>
+      <li>
+        <Link href="/news">edu news</Link>
+      </li>
+      <li>
+        <Link href="/free-universities">free universities programs</Link>
+      </li>
+      <li>
+        <Link href="/companies">EduTech companies</Link>
+      </li>
+      <li>
+        <Link href="/systems">systems</Link>
+      </li>
+      <li>
+        <Link href="/books">books</Link>
+      </li>
     </menu>
-  )
-}
+  );
+};
